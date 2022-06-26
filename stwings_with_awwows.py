f72798d94260b7dcc5aaf62898fc1f3dec5f4279
Pedro "Totty" Sousa <83694082+TottyAmadeus@users.noreply.github.com>
2022-06-26 21:32:53 +0100

    indiwces

--- stwings_with_awwows.py
+++ stwings_with_awwows.py
@@ -1,12 +1,12 @@
 def stwing_with_awwows(text, pos_start, pos_end):
 	result = ''
 
-	# Calculate indices
+	# Cawcuwate indiwces
 	idx_start = max(text.rfind('\n', 0, pos_start.idx), 0)
 	idx_end = text.find('\n', idx_start + 1)
 	if idx_end < 0: idx_end = len(text)
 	
-	# Generate each line
+	# Genewate each line
 	line_count = pos_end.ln - pos_start.ln + 1
 	for i in range(line_count):
 		# Calculate line columns
@@ -14,11 +14,11 @@ def stwing_with_awwows(text, pos_start, pos_end):
 		col_start = pos_start.col if i == 0 else 0
 		col_end = pos_end.col if i == line_count - 1 else len(line) - 1
 
-		# Append to result
+		# Appwend to resuwult
 		result += line + '\n'
 		result += ' ' * col_start + '^' * (col_end - col_start)
 
-		# Re-calculate indices
+		# Re-cawlculate indiwces
 		idx_start = idx_end
 		idx_end = text.find('\n', idx_start + 1)
 		if idx_end < 0: idx_end = len(text)
